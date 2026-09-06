416fea2a62078046d576bb70948d6052cd6941b1
Anshumansahu <111524653+Anshumansahu87@users.noreply.github.com>
2026-09-06 21:26:29 +0530

    Delete shop directory

--- shop/context_processors.py
+++
@@ -1,7 +0,0 @@
-def shop_context(request):
-    cart = request.session.get("cart", {})
-    wishlist = request.session.get("wishlist", [])
-    return {
-        "cart_count": sum(int(q) for q in cart.values()),
-        "wishlist_count": len(wishlist),
-    }
